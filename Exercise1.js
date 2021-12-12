b12dbc2fe17269ea85dc5265303c57c9b360fa26
ragabmoh2ooo <ragabmoh2ooo>
2021-12-12 23:14:50 +0200

    Exercise1

---
+++ Exercise1.js
@@ -0,0 +1,22 @@
+/*
+  Write a program javascript to check two number if one of them equal 100 the return true or the sum of two numbers is 100 then.
+*/
+/** Pseudo Code */
+
+// Enter Two Numbers
+debugger 
+let $Num1=parseInt(prompt("Enter First Number:"));
+let $Num2=parseInt(prompt("Enter Second Number :"))
+// Check if one of them equals 100 or sum of Numbers are 100
+debugger 
+if($Num1==100){
+    alert("Number One equals 100");
+}else if($Num2==100){
+    alert("Number Two equals 100");
+}else if($Num1+$Num2==100){
+    alert(" Sum of Two  Numbers are equaling 100");
+}else{
+    alert("Invalid Two Numbers Try Again...!!");
+}
+// By using Ragab Mohamed Ragab
+// We can contact at WhatsApp : +20 01121237611
