88fb2df97cc4f710d276347f42aae122d3c6afee
Ragab Muhamed Ragab <64719825+ragabmoh2ooo@users.noreply.github.com>
2021-12-21 01:36:44 +0200

    Update Exercies3.js

--- Exercies3.js
+++ Exercies3.js
@@ -1,17 +1,14 @@
 // Write a program javascript to replace evey character in given string to following it in alphabets
 // Date => 20-12-2021 At 10:31 PM
 export function character() {
-  
+    debugger
     let $GivenStr = prompt("Enter String :");
-    let AnewStr = (str) => {
-        var Lexical = "";
-        for (var item in str) {
+    var Lexical="";
+    for (var item in $GivenStr) {
 
-            Lexical += String.fromCharCode((str.charCodeAt(item) + 1));
-        };
-        return Lexical;
-    }
-
+        Lexical += String.fromCharCode(($GivenStr.charCodeAt(item) + 1));
+    };
+    return Lexical;
 }
 // By using Ragab Mohamed Ragab
 // You can contact  Me at WhatsApp : +20 01121237611
