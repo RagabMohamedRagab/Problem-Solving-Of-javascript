de77fae2d137a7058be9ed3bc057c1e5c2b8d69d
ragabmoh2ooo <ragabmoh2ooo>
2021-12-21 00:20:59 +0200

    AddAnewFilesAndModifiedIt

---
+++ Main.js
@@ -0,0 +1,11 @@
+import {Check} from './Exercise1.js';
+import {Extention} from './Exercise2.js';
+import {character} from './Exercise3.js'; 
+
+// First Import 
+ Check();
+ // Second Import
+ Extention();
+ // Third Import 
+ console.log(character());
+
