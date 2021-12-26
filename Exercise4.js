7db8ded1237a23fa233cb77aed92c5593e30978a
Ragab Muhamed Ragab <64719825+ragabmoh2ooo@users.noreply.github.com>
2021-12-27 00:11:34 +0200

    Update Exercise4.js

--- Exercise4.js
+++ Exercise4.js
@@ -7,3 +7,7 @@ export class GetFullTime {
         return `The Day :${new Date().getDate()} -The Month:  ${new Date().getMonth()} -The Year: ${new Date().getFullYear()} \nThe Hour: ${new Date().getHours()} -The Minute: ${ new Date().getMinutes()} -The Second : ${new Date().getSeconds()}`
     }
 }
+
+
+// By using Ragab Mohamed Ragab
+// You can contact  Me at WhatsApp : +20 01121237611
